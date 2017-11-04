e9acf0f33a97a6f5cb0c11f6c54429bbb1150285
Andy Lin <andylin1218@gmail.com>
2017-11-04 03:55:25 -0700

    Added tests to for task list

---
+++ tests/TaskListTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
+}
+
+class TaskListTest extends TestCase {
+    private $CI;
+
+    public function setUp() {
+        // Load CI instance normally
+        $this->CI = &get_instance();
+    }
+
+    public function testUncompletedTasksGreaterThanCompleted() {
+        $numOfUncompleted = 0;
+        $tasks = $this->CI->tasks->all();
+
+        foreach ($tasks as $task) {
+            if ($task->status != 2) {
+                $numOfUncompleted++;
+            }
+        }
+
+        $this->assertGreaterThan(count($tasks)/2, $numOfUncompleted);
+    }
+}
