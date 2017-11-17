52fb7f7e9462e454023155af9df1bc418a4e2e9e
Andy Lin <andylin1218@gmail.com>
2017-11-17 12:30:12 -0800

    Added XML_Model with correct load

---
+++ application/core/XML_Model.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * XML-persisted collection.
+ *
+ * ------------------------------------------------------------------------
+ */
+class XML_Model extends Memory_Model
+{
+//---------------------------------------------------------------------------
+//  Housekeeping methods
+//---------------------------------------------------------------------------
+
+    /**
+     * Constructor.
+     * @param string $origin Filename of the XML file
+     * @param string $keyfield  Name of the primary key field
+     * @param string $entity    Entity name meaningful to the persistence
+     */
+    function __construct($origin = null, $keyfield = 'id', $entity = null)
+    {
+        parent::__construct();
+
+        // guess at persistent name if not specified
+        if ($origin == null)
+            $this->_origin = get_class($this);
+        else
+            $this->_origin = $origin;
+
+        // remember the other constructor fields
+        $this->_keyfield = $keyfield;
+        $this->_entity = $entity;
+
+        // start with an empty collection
+        $this->_data = array(); // an array of objects
+        $this->_fields = array(); // an array of strings
+        // and populate the collection
+        $this->load();
+    }
+
+    /**
+     * Load the collection state appropriately, depending on persistence choice.
+     * OVER-RIDE THIS METHOD in persistence choice implementations
+     */
+    protected function load()
+    {
+        //---------------------
+        if (($data = simplexml_load_file($this->_origin)) !== FALSE)
+        {
+            $temp = new stdClass();
+            $this->loadChild($data, $temp);
+        }
+        // --------------------
+        // rebuild the keys table
+        $this->reindex();
+    }
+
+    private function loadChild($xml, &$record)
+    {
+        $child_count = 0;
+        foreach($xml as $key => $value)
+        {
+            $temp = new stdClass();
+            $child_count++;
+            if($this->loadChild($value, $temp) == 0)  // no childern, aka "leaf node"
+            {
+                if (!in_array($key, $this->_fields)) {
+                    array_push($this->_fields, $key);
+                }
+
+                $record->$key = ((string)$value);
+            } else {
+                $recordKey = $temp->{$this->_keyfield};
+                $this->_data[$recordKey] = $temp;
+            }
+        }
+        return $child_count;
+    }
+
+    /**
+     * Store the collection state appropriately, depending on persistence choice.
+     * OVER-RIDE THIS METHOD in persistence choice implementations
+     */
+    protected function store()
+    {
+        // rebuild the keys table
+        $this->reindex();
+        //---------------------
+        // --------------------
+    }
+
+}
